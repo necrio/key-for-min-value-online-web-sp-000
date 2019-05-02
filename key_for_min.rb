b0ead6c0b624d9552f83d38ca6455f34ffbf415f
Keith Isham <kisham8899@gmail.com>
2019-05-02 09:49:29 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,15 +4,14 @@
 name_hash =     {:blake => 500, :ashley => 2, :adam => 1}
 
 def key_for_min_value(name_hash)
+low_val = 0
+low_key = nil
 
-
-name_hash.each do |name, value|
-  if value < 2
-    return name
-  else
-    return nil
+name_hash.each do |names, num|
+  if low_val == 0 || num < low_val
+    low_val = num
+    low_key = names
   end
 end
-
-
+low_key
 end
