c2ac59973c2b0ccff24f1789aa630dc122a48759
Keith Isham <kisham8899@gmail.com>
2019-05-02 09:25:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,8 +1,12 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-names =     {:black => 500, :ashley => 2, :adam +> 1}
+
 
 def key_for_min_value(name_hash)
+names =     {:blake => 500, :ashley => 2, :adam +> 1}
+
+
+  
 
 end
